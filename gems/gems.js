3d9f8116011f83dc79bcdb3b6b2c40e79ba1db37
Paul Wand <pwand@accesssoftek.com>
2015-04-08 20:20:09 -0700

    version de cache

--- gems/gems.js
+++ gems/gems.js
@@ -4,7 +4,7 @@ angular.module('myApp.gems', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/gems', {
-    templateUrl: 'gems/gems.html',
+    templateUrl: 'gems/gems.html?version=0.0.0',
     controller: 'gemsCtrl'
   });
 }])
--- jams/jams.js
+++ jams/jams.js
@@ -4,7 +4,7 @@ angular.module('myApp.jams', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/jams', {
-    templateUrl: 'jams/jams.html',
+    templateUrl: 'jams/jams.html?version=0.0.0',
     controller: 'JamsCtrl'
   });
 }])
